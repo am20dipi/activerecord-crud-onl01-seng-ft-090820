5aa5b17fed9c9b9619ee63b6497119d0d03fc0aa
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-13 00:19:55 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -27,7 +27,7 @@ def can_be_created_in_a_block
   # title == "Home Alone"
   # release_date == 1990
   
-  Movie.create do |m|
+  Movie.create do {|m| m.title, m.release_date}
     m.title = "Home Alone"
     m.release_date = 1990
   end
