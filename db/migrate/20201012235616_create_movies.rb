c5b399c3c7370e41f4f17c15886afa5365833149
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-13 00:00:38 +0000

    Automatically backed up by Learn

--- db/migrate/20201012235616_create_movies.rb
+++ db/migrate/20201012235616_create_movies.rb
@@ -1,9 +1,6 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
-    add_column :movies, :title, :string
-    add_column :movies, :release_date, :integer
-    add_column :movies, :director, :string
-    add_column :movies, :lead, :string
-    add_column :movies, :in_theaters, :boolean
+    create_table :movies do |t|
+      t.string :title
   end
 end
